a8039231895ed067e228c28e8c8fcbb38f2f8b7b
João <87874652+itsjoaaaao@users.noreply.github.com>
2021-11-02 01:37:44 -0300

    Add files via upload

---
+++ Tasks.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Task from "./Task";
+const Tasks = ({tasks, handleTaskClick, handleTaskDeletion}) =>{
+    return (
+    <div>
+        {tasks.map((task) => (
+            <Task task={task} 
+            handleTaskClick={handleTaskClick} 
+            handleTaskDeletion={handleTaskDeletion}/>
+        ))}
+    </div>
+
+    );
+};
+
+export default Tasks;
